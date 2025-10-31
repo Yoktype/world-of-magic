ca6fbe2122ec1665e67144ccb6d4d68d33beb631
Yok <233425415+Yoktype@users.noreply.github.com>
2025-10-31 22:30:13 +0300

    first info TODO and base tasks

---
+++ src/server/TODO.ts
@@ -0,0 +1,20 @@
+/*
+world-of-magic
+GAME:
+    - Staff w. Abilitys (diff. staffs);
+    - Armore (give: Health, Walkspeed);
+    - Inventory;
+    - Store (buy Staffs and Armores);
+    - Level-system;
+    - Money-take, Money and exp for kill;
+    - Data save;
+
+TODO:
+    - data handler (ProfileStore);
+    - base init progect (folders, base utils);
+        - Types;
+        - Assets, Events;
+        - Util initialize;
+        - 
+
+*/
